b10b900d3af91fe38e2cc912593e7f9d5a9cca8b
etta1509 <s328422@studenti.polito.it>
2025-03-20 12:49:22 +0100

    lab3-update

--- dataset/transform_dataset.py
+++ dataset/transform_dataset.py
@@ -32,7 +32,7 @@ def transform_2():
     return transform
 
 def modify_dataset():
-    with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+    with open('./dataset/tiny-imagenet-200/val/val_annotations.txt') as f:
         for line in f:
             fn, cls, *_ = line.split('\t')
             os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
