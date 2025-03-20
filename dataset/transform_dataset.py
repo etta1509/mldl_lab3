cfe5d6d9ce1e3289f43da7bbcb9c600fdead2e1b
etta1509 <s328422@studenti.polito.it>
2025-03-20 12:50:42 +0100

    lab3-updates

--- dataset/transform_dataset.py
+++ dataset/transform_dataset.py
@@ -35,7 +35,7 @@ def modify_dataset():
     with open('./dataset/tiny-imagenet-200/val/val_annotations.txt') as f:
         for line in f:
             fn, cls, *_ = line.split('\t')
-            os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
-            shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+            os.makedirs(f'./dataset/tiny-imagenet-200/val/{cls}', exist_ok=True)
+            shutil.copyfile(f'./dataset/tiny-imagenet-200/val/images/{fn}', f'./dataset/tiny-imagenet-200/val/{cls}/{fn}')
 
-    shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
+    shutil.rmtree('./dataset/tiny-imagenet-200/val/images')
